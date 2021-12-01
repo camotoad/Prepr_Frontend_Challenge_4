f4ea157cb0f7ea238abf564266d4628f12aa1b77
Gan <camotoad1@gmail.com>
2021-11-30 21:08:49 -0500

    Functionalities (check details)
    Closing, moving menu.
    Toggle switches.
    Reset button.
    Language menu.
    Overlay.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -83,3 +83,159 @@ $('#profile-nav').on('click',()=>{
         $('#right-nav').addClass('profile-nav-open')
     }
 });
+
+$('#lang-close').on('click', ()=>{
+    $('#lang-select').hide();
+});
+
+$('.acsb-lang').on('click', () =>{
+    $('#lang-select').show();
+});
+
+$('#move-btn').on('click', () => {
+    if($('#acsb-menu').hasClass('moved') && $('#lang-select').hasClass('moved')){
+        $('#acsb-menu').removeClass('moved');
+        $('#lang-select').removeClass('moved');
+    } else {
+        $('#acsb-menu').addClass('moved');
+        $('#lang-select').addClass('moved');
+    }
+})
+
+$('#close-btn').on('click', () => {
+    $('#acsb-menu').hide()
+});
+
+$('#reset-btn').on('click', () => {
+    location.reload();
+});
+
+$('#acsb-toggle').on('click', () => {
+    $('#acsb-menu').show();
+    $('.overlay').addClass('active');
+});
+
+$('.overlay').on('click', () => {
+    $('#acsb-menu').hide()
+    $('#lang-select').hide();
+    $('.overlay').removeClass('active');
+});
+
+$('#lang-select').on('click', (event) => {
+    let clickedElement = event.target.id;
+
+    switch(clickedElement){
+        case 'English':
+        case 'Spanish':
+        case 'German':
+        case 'Portugese':
+        case 'French':
+        case 'Italian':
+        case 'Hebrew':
+        case 'Chinese-SIMP':
+        case 'Russian':
+        case 'Arabic':
+        case 'Dutch':
+        case 'Chinese-TRAD':
+        case 'Japanese':
+                $('#language').text(clickedElement);
+                $('#lang-select').hide();
+                break;
+        default:
+            return false;
+    }
+});
+
+$('#profile1').on('click', () =>{
+    if($('#p1').hasClass('moved')){
+        $('#p1').removeClass('moved');
+        $('#p1text').hide();
+    } else{
+        resetSwitches();
+        $('#p1').addClass('moved');
+        $('#p1text').show();
+    }
+});
+
+$('#profile2').on('click', () =>{
+    if($('#p2').hasClass('moved')){
+        $('#p2').removeClass('moved');
+        $('#p2text').hide();
+    } else{
+        resetSwitches();
+        $('#p2').addClass('moved');
+        $('#p2text').show();
+    }
+});
+
+$('#profile3').on('click', () =>{
+    if($('#p3').hasClass('moved')){
+        $('#p3').removeClass('moved');
+        $('#p3text').hide();
+    } else{
+        resetSwitches();
+        $('#p3').addClass('moved');
+        $('#p3text').show();
+    }
+});
+
+$('#profile4').on('click', () =>{
+    if($('#p4').hasClass('moved')){
+        $('#p4').removeClass('moved');
+        $('#p4text').hide();
+    } else{
+        resetSwitches();
+        $('#p4').addClass('moved');
+        $('#p4text').show();
+    }
+});
+
+$('#profile5').on('click', () =>{
+    if($('#p6').hasClass('moved')){
+        $('#p5').removeClass('moved');
+        $('#p5text').hide();
+        $('#p6').removeClass('moved');
+        $('#p6text').hide();
+    } else{
+        resetSwitches();
+        $('#p5').addClass('moved');
+        $('#p5text').show();
+        $('#p6').addClass('moved');
+        $('#p6text').show();
+    }
+});
+
+$('#profile6').on('click', () =>{
+    if($('#p6').hasClass('moved')){
+        $('#p5').removeClass('moved');
+        $('#p5text').hide();
+        $('#p6').removeClass('moved');
+        $('#p6text').hide();
+    } else{
+        resetSwitches();
+        $('#p5').addClass('moved');
+        $('#p5text').show();
+        $('#p6').addClass('moved');
+        $('#p6text').show();
+    }
+});
+
+function resetSwitches(){
+    $('#p1').removeClass('moved');
+    $('#p1text').hide();
+
+    $('#p2').removeClass('moved');
+    $('#p2text').hide();
+
+    $('#p3').removeClass('moved');
+    $('#p3text').hide();
+
+    $('#p4').removeClass('moved');
+    $('#p4text').hide();
+
+    $('#p5').removeClass('moved');
+    $('#p5text').hide();
+
+    $('#p6').removeClass('moved');
+    $('#p6text').hide();
+}
